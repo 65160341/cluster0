37a5ff324581145b541e9fd7ef480e5c1ba325d1
65160321 <65160321@go.buu.ac.th>
2024-04-07 09:18:08 +0700

    Create formdetail.blade.php

---
+++ resources/views/formdetail.blade.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ระบบรับสมัครพนักงงาน</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="styleTest.css"> --}}
+    <link rel="stylesheet" href="{{ asset('asset\dist\css\page.css') }}">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/css/tempusdominus-bootstrap-4.min.css" />
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class='bx bx-menu'></i>
+                </button>
+
+                <div class="sidebar-logo">
+                    <span class="image">
+                        <img src="https://blog.clicknext.com/wp-content/themes/clicknext_blog2/assets/images/clicknext-logo.png"
+                            alt="logo">
+                    </span>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class='bx bxs-dashboard'></i>
+                        <span>แดชบอร์ด</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
+                        aria-expanded="false" aria-controls="auth">
+                        <i class='bx bxs-food-menu'></i>
+                        <span>ฟอร์ม</span>
+                    </a>
+                    <div id="auth" class="sidebar-dropdown collapse">
+                        <ul class="list-unstyled">
+                            <li class="sidebar-item">
+
+                                <a href="#" class="sidebar-link"><i
+                                        class='bx bx-chevron-right'></i>สร้างฟอร์มรับสมัคร</a>
+                            </li>
+                            <li class="sidebar-item">
+
+                                <a href="#" class="sidebar-link"><i
+                                        class='bx bx-chevron-right'></i>ฟอร์มของฉัน</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class='bx bxs-info-circle'></i>
+                        <span>ข้อมูล</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class='bx bx-log-out'></i>
+                    <span>ออกจากระบบ</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main">
+            <nav class="navbar navbar-expand-lg px-4 py-3">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="Unknown_person.jpg" class="avatar img-fluid" alt="">Username
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">โปรไฟล์</a></li>
+                                <li><a class="dropdown-item" href="#">ตั้งค่า</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">ออกจากระบบ</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="content px-3 py-4">
+                <div class="container-fluid">
+                    <div class="mb-3">
+
+                        <div class="row align-items-center mb-3">
+                            <div class="col-auto">
+                                <span>ปีที่เปิดรับสมัคร</span>
+                            </div>
+                            <div class="col-auto">
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button"
+                                        id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        ปีที่รับสมัคร
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="#">2567</a></li>
+                                        <li><a class="dropdown-item" href="#">2566</a></li>
+                                        <li><a class="dropdown-item" href="#">2565</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <span>ระยะเวลารับสมัคร</span>
+                            </div>
+                            <div class="col-auto">
+                                <button class='btn col-auto bx bxs-calendar' style="font-size: 30px"></button>
+                            </div>
+                            <div class="col-auto">
+                                <div class="input-group" id="dateRangePicker">
+                                    <button class="btn btn-outline-secondary" type="button" id="dateRange">
+                                        <i class='bx bxs-calendar' style="font-size: 30px"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <div data-coreui-locale="en-US" data-coreui-toggle="date-range-picker"></div>
+                                </div>
+                                <div class="col-lg-5">
+                                    <div data-coreui-start-date="2022/08/03" data-coreui-end-date="2022/08/17"
+                                        data-coreui-locale="en-US" data-coreui-toggle="date-range-picker">
+                                    </div>
+                                </div>
+                            </div>
+                            <input type="text" name="daterange" value="01/01/2018 - 01/15/2018" />
+                        </div>
+                        <div class="col-12 columnset">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th width="150px">รอบการรับสมัคร</th>
+                                        <th width="200px">รายละเอียด</th>
+                                        <th width="200px">วันที่เปิดรับ</th>
+                                        <th width="200px">วันที่สิ้นสุด</th>
+                                        <th width="200px">
+                                            <li class="nav-item dropdown">
+                                                <a href="#" class="nav-link dropdown-toggle"
+                                                    id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    <class= alt="">สถานะ
+                                                </a>
+                                                <ul class="dropdown-menu dropdown-menu-end"
+                                                    aria-labelledby="navbarDropdown">
+                                                    <li><a class="dropdown-item" href="#">เปิดรับสมัคร</a></li>
+                                                    <li><a class="dropdown-item" href="#">ปิดรับสมัคร</a></li>
+                                                </ul>
+                                            </li>
+                                        </th>
+                                        <td width="200px"></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">2 / 2567</th>
+                                        <td>รับสมัครที่มหาวิทยาลัยบูรพา วันที่ 6 เดือนเมษายน พ.ศ. 2567
+                                            คณะวิทยาการสารสนเทศ</td>
+                                        <td>11/03/2567</td>
+                                        <td>25/03/2567</td>
+                                        <td>ปิดรับสมัคร</td>
+                                        <td><button class="btn btn-primary">ตรวจสอบ</button>
+                                            <button class="btn btn-danger">ลบ</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </main>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const hamBurger = document.querySelector(".toggle-btn");
+
+        hamBurger.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("expand");
+        });
+        $(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+    </script>
+</body>
+
+</html>
