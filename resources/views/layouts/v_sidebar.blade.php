03a1e1f0eb1304f0c7f21b17053a215d7a9adbfb
65160321 <65160321@go.buu.ac.th>
2024-04-09 12:53:53 +0700

    Update v_sidebar.blade.php

---
+++ resources/views/layouts/v_sidebar.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ระบบรับสมัครพนักงงาน</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="styleTest.css"> --}}
+    <link rel="stylesheet" href="{{ asset('asset\dist\css\page.css') }}">
+    <link
+        rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/css/tempusdominus-bootstrap-4.min.css" />
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class='bx bx-menu'></i>
+                </button>
+
+                <div class="sidebar-logo">
+                    <span class="image">
+                        <img src="https://blog.clicknext.com/wp-content/themes/clicknext_blog2/assets/images/clicknext-logo.png"
+                            alt="logo">
+                    </span>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class='bx bxs-dashboard'></i>
+                        <span>แดชบอร์ด</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
+                        aria-expanded="false" aria-controls="auth">
+                        <i class='bx bxs-food-menu'></i>
+                        <span>ฟอร์ม</span>
+                    </a>
+                    <div id="auth" class="sidebar-dropdown collapse">
+                        <ul class="list-unstyled">
+                            <li class="sidebar-item">
+
+                                <a href="#" class="sidebar-link"><i
+                                        class='bx bx-chevron-right'></i>สร้างฟอร์มรับสมัคร</a>
+                            </li>
+                            <li class="sidebar-item">
+
+                                <a href="#" class="sidebar-link"><i
+                                        class='bx bx-chevron-right'></i>ฟอร์มของฉัน</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class='bx bxs-info-circle'></i>
+                        <span>ข้อมูล</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class='bx bx-log-out'></i>
+                    <span>ออกจากระบบ</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main">
+            <nav class="navbar navbar-expand-lg px-4 py-3">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="Unknown_person.jpg" class="avatar img-fluid" alt="">Username
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">โปรไฟล์</a></li>
+                                <li><a class="dropdown-item" href="#">ตั้งค่า</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">ออกจากระบบ</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main>@yield('content')</main>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const hamBurger = document.querySelector(".toggle-btn");
+
+        hamBurger.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("expand");
+        });
+    </script>
+
+
+</body>
+
+</html>
