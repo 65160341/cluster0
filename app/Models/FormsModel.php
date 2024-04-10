351da3932f9584e912e75102aeb1ff0cbaf106b3
65160321 <65160321@go.buu.ac.th>
2024-04-10 14:18:29 +0700

    Delete FormsModel.php

--- app/Models/FormsModel.php
+++ app/Models/FormsModel.php
@@ -5,18 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FormsModel extends Model
+class formsModel extends Model
 {
     use HasFactory;
-
-    protected $table ='Testforms';
-    protected $primaryKey = 'Testforms_id';
-
-    // protected $fillable = [
-    //     'form_id',
-    //     'form_round_count',
-    //     'form_round_year',
-    //     'form_date_start',
-    //     'form_date_end'
-    // ];
+    Protected $table = 'forms';
+    protected $primaryKey = 'form_id';
 }
